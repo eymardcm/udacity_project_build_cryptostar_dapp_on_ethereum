983a2c6def164f1154a646fe173f2f7e34d236cf
Chad <eymardcm@gmail.com>
2019-06-11 22:28:47 -0500

    First Commit

---
+++ truffle-config.js
@@ -0,0 +1,44 @@
+const infuraKey = '6e0215354b9241ae940809ca64531674';
+const mnemonic = 'craft field agent judge receive adult exile sound grid vendor section erode';
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+module.exports = {
+  networks: {
+    development: {
+      host: '127.0.0.1', // Localhost (default: none)
+      port: 9545, // Standard Ethereum port (default: none)
+      network_id: '*' // Any network (default: none)
+    },
+
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://rinkeby.infura.io/v3/${infuraKey}`
+        ),
+      network_id: 4, // rinkeby's id
+      gas: 4500000, // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
+    }
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+};
